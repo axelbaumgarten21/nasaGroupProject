ac522f10affb31e656a4c266ae5b6c19020627c5
axelbaumgarten21 <axelbaumgarten21@gmail.com>
2021-08-08 10:27:48 -0500

    updates

--- src/__tests__/nasaSpecs.ts
+++ src/__tests__/nasaSpecs.ts
@@ -13,6 +13,13 @@ homepage: By = By.xpath("//*[@class='html front not-logged-in page-indexhtml sho
 constructor(driver: WebDriver) {
   this.driver = driver;
 }
+async navigate() {
+  await this.driver.get(this.url);
+  await this.driver.wait(until.elementLocated(this.homepage));
+  await this.driver.wait(
+    until.elementIsVisible(await this.driver.findElement(this.homepage))
+  );
+}
 async getText(elementBy: By) {
     await this.driver.wait(until.elementLocated(elementBy));
     return (await this.driver.findElement(elementBy)).getText();
@@ -20,4 +27,5 @@ async getText(elementBy: By) {
   async getResults() {
     return this.getText(this.homepage);
   }
+  
 }
