44734f73cfefdc95d37887bd39f67f9b6ef2e495
axelbaumgarten21 <axelbaumgarten21@gmail.com>
2021-08-11 12:49:12 -0500

    complete nasaSpecs changes- please fetch and pull

--- src/__tests__/nasaSpecs.ts
+++ src/__tests__/nasaSpecs.ts
@@ -1,14 +1,19 @@
 import { By, until, WebDriver } from "selenium-webdriver";
+// const foto = require('foto');
 
-
-    export class nasaSpecs {
-        driver: WebDriver;
-        url: string = "https://www.nasa.gov/";
+export class nasaSpecs {
+  static getSuccess(): any {
+    throw new Error("Method not implemented.");
+  }
+    driver: WebDriver;
+    url: string = "https://www.nasa.gov/";    
 
 //TB-added searchbar and searchresults locator
 homepage: By = By.xpath("//*[@class='html front not-logged-in page-indexhtml show-topics-menu ember-application']");
+successP: By = By.xpath('//*[@id="content"]/div'); //("static-landing-page");
 searchBar: By = By.xpath('//*[@name="query"]');
 searchresults: By = By.xpath("//*[@id='best-bet-1']")
+pdf: By = By.xpath('//*[@id="ember20"]/div[1]/p[5]/a[2]');
 
 
 constructor(driver: WebDriver) {
@@ -36,6 +41,13 @@ async sendKeys(elementBy: By, keys) {
 async getsearchresults() {
   return this.getText(this.searchresults)
 }
+async getSuccess() {
+  return this.getText(this.successP);
+}
+
+async getPdf() {
+  return this.getText(this.pdf);
+}
 
 //TB-adding a doSearch function
 async doSearch (searchTerm) {
@@ -45,4 +57,15 @@ async doSearch (searchTerm) {
   await this.getsearchresults();
   expect(myText).toContain(`${searchTerm}`);
   }
+      // async takeScreenshot(filepath: string) {
+    //   foto.writeFile(
+    //     `${filepath}.png`,
+    //     await this.driver.takeScreenshot(),
+    //     "base64",
+    //     (e) => {
+    //       if (e) console.log(e);
+    //       else console.log("screenshot saved successfully");
+    //     }
+    //   );
+    // }
 }
