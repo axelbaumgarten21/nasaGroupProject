5c38aff4cb6b51c955683d9dbac4f49baedad4d1
Yulia <iuliiabarbash@gmail.com>
2021-08-11 20:39:05 -0400

    file specs updated

--- src/__tests__/nasaSpecs.ts
+++ src/__tests__/nasaSpecs.ts
@@ -1,21 +1,20 @@
 import { By, until, WebDriver } from "selenium-webdriver";
 // const foto = require('foto');
 
-    export class nasaSpecs {
-      static getSuccess(): any {
-        throw new Error("Method not implemented.");
-      }
-        driver: WebDriver;
-        url: string = "https://www.nasa.gov/";  
+export class nasaSpecs {
+  static getSuccess(): any {
+    throw new Error("Method not implemented.");
+  }
+    driver: WebDriver;
+    url: string = "https://www.nasa.gov/";    
 
-
+//TB-added searchbar and searchresults locator
 homepage: By = By.xpath("//*[@class='html front not-logged-in page-indexhtml show-topics-menu ember-application']");
 successP: By = By.xpath('//*[@id="content"]/div'); //("static-landing-page");
 searchBar: By = By.xpath('//*[@name="query"]');
-searchresults: By = By.xpath("//*[@id='best-bet-1']"); 
+searchresults: By = By.xpath("//*[@id='best-bet-1']")
 pdf: By = By.xpath('//*[@id="ember20"]/div[1]/p[5]/a[2]');
 
-      
 
 constructor(driver: WebDriver) {
   this.driver = driver;
@@ -28,36 +27,37 @@ async navigate() {
   );
 }
 async getText(elementBy: By) {
-    await this.driver.wait(until.elementLocated(elementBy));
-    return (await this.driver.findElement(elementBy)).getText();
-  }
-
-  async sendKeys(elementBy: By, keys) {
-    await this.driver.wait(until.elementsLocated(elementBy));
-    return this.driver.findElement(elementBy).sendKeys(keys);
+  await this.driver.wait(until.elementLocated(elementBy));
+  return (await this.driver.findElement(elementBy)).getText();
 }
-  async getResults() {
+async sendKeys(elementBy: By, keys) {
+  await this.driver.wait(until.elementLocated(elementBy));
+  return this.driver.findElement(elementBy).sendKeys(keys);
+}
+        async getResults() {
     return this.getText(this.homepage);
   }
+//TB-added getsearchresults function
+async getsearchresults() {
+  return this.getText(this.searchresults)
+}
+async getSuccess() {
+  return this.getText(this.successP);
+}
 
-  async getSuccess() {
-    return this.getText(this.successP);
+async getPdf() {
+  return this.getText(this.pdf);
+}
+
+//TB-adding a doSearch function
+async doSearch (searchTerm) {
+  let search = await this.driver.findElement(this.searchBar)
+  await this.sendKeys(this.searchBar, `${searchTerm}\n`)
+  let myText = await this.driver.findElement(this.searchresults).getText();
+  await this.getsearchresults();
+  expect(myText).toContain(`${searchTerm}`);
   }
-
-  async getPdf() {
-    return this.getText(this.pdf);
-  }
-
-  async doSearch (searchTerm: string) {
-    await this.driver.wait(until.elementLocated(this.searchBar))
-    let search = await this.driver.findElement(this.searchBar)
-    await this.sendKeys(this.searchBar, `${searchTerm}\n`)
-    let myText = await this.driver.findElement(this.searchresults).getText();
-    expect(myText).toContain(`${searchTerm}`);
-    }
-
-
-    // async takeScreenshot(filepath: string) {
+      // async takeScreenshot(filepath: string) {
     //   foto.writeFile(
     //     `${filepath}.png`,
     //     await this.driver.takeScreenshot(),
@@ -68,6 +68,4 @@ async getText(elementBy: By) {
     //     }
     //   );
     // }
-  
-
 }
